d1b29ed75f1db4187cbb6d8a85d74c72901d3d9f
MothyLag <decagufy30@gmail.com>
2022-05-02 17:09:22 -0500

    uploading proyect

---
+++ Aumenta Dot Net Academy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aumenta_Dot_Net_Academy
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
